6cf9d6db19b1969abe2162c5e19f8c4f6c817764
wangzhen <wangzhen@liepiaowang.com>
2018-03-01 16:55:18 +0800

    merged commits

--- src/main/java/xin/bobospace/BobospaceApplication.java
+++ src/main/java/xin/bobospace/BobospaceApplication.java
@@ -2,7 +2,8 @@ package xin.bobospace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+////
+//abc
 @SpringBootApplication
 public class BobospaceApplication {
 
